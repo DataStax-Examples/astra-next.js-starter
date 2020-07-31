c04cd0849700e91c6bf6470e4cfee784368fae6e
Alex Leventer <alexleventer@gmail.com>
2020-07-31 12:22:51 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { getCharacters } from '../lib/astra'
 
-export default function Home() {
+export default function Home({ characters }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,45 +12,24 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Simple Next.js + <a href="https://www.astra.datastax.com/register">Astra</a> sample!
         </h1>
-
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {characters.map(character => {
+            return (<div key={character.id} className={styles.card}>
+              <h3>{character.name}</h3>
+              <p>
+                  <b>Actor:</b> {character.actorname}<br/>
+                  <b>House:</b> {character.housename}
+              </p>
+            </div>);
+          })}
         </div>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -62,4 +42,11 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps({ preview = false }) {
+  const characters = (await getCharacters()) || []
+  return {
+    props: { characters, preview },
+  }
 }
